e0186c48442f1c8ada9625c315862ade19929326
Katherine <kvsierrab@unal.edu.co>
2018-09-03 21:03:33 +0000

    Library

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,37 @@
+class BooksController < ApplicationController
+    
+    def index
+        books = Book.all
+        respond_to do |format| 
+            format.json {render json: "{\"name\":\"Alejandro\"}", status:200}
+            format.html {render html: "{\"name\":\"Alejandro\"}", status:200}
+
+        end
+    end
+    
+    def create
+        book = Book.new(params_book)
+        if book.save
+            respond_to do |format| 
+            format.json {render json: "{\"name\":\"Alejandro\"}", status:201}
+            end
+        else
+            respond_to do |format| 
+            format.json {render json: book.errors, status: unprocessable_enty}
+            end
+        end
+    end
+    
+    def params_book
+        params.permit(:name, :year, :code, :available)
+    end    
+    
+    def show
+        book = Book.find(params[:id])
+        respond_to do |format| 
+            format.json {render json: book, status:200}
+            format.html {render html: book, status:200}
+
+        end
+    end
+end
